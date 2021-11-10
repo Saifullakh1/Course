e69e57e5281c310819ede930559550181c06b614
Saifullakh <jundev74@gmail.com>
2021-11-10 13:36:51 +0600

    add amazon s3

--- tains/tains/settings.py
+++ tains/tains/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'storages',
+
     # register with google
     'django.contrib.sites',
     'allauth',
@@ -153,7 +155,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
@@ -162,4 +163,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# S3 BUCKETS CONFIG
+
+
+AWS_ACCESS_KEY_ID = 'AKIARZV6U7G57U5JH4W4'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'courses-1'
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = None
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
